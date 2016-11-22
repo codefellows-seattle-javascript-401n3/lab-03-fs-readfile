db46cb1aaa05b7a3f5f238c0752b192bffe7b79d
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-11-21 22:39:39 -0800

    Changed to deepEqual

--- lab-jessica/lib/read.js
+++ lab-jessica/lib/read.js
@@ -1,30 +1,23 @@
 const fs = require('fs');
 
 function read(callback) {
-
   let files = [];
-
   fs.readFile('./read-me/one.txt', function(err, data) {
     let file1 = 'file 1 ' + data.toString('hex', 0, 8);
     files.push(file1);
     console.log(file1);
-
     fs.readFile('./read-me/two.txt', function(err, data) {
       let file2 = 'file 2 ' + data.toString('hex', 0, 8);
       files.push(file2);
       console.log(file2);
-
       fs.readFile('./read-me/three.txt', function(err, data) {
         let file3 = 'file 3 ' + data.toString('hex', 0, 8);
         files.push(file3);
         console.log(file3);
-        console.log(files);
         callback(files);
       });
-
     });
   });
-
 }
 
 module.exports = read;
--- lab-jessica/test/test.js
+++ lab-jessica/test/test.js
@@ -5,13 +5,9 @@ describe('Read Module', function() {
 
   describe('#read()',  function() {
     it('files logged in correct order', function(done) {
-      var files = ['file 1 5b0a20207b0a2020', 'file 2 6c61622d30332d66', 'file 3 4c6f72656d206970'];
+      const files = ['file 1 5b0a20207b0a2020', 'file 2 6c61622d30332d66', 'file 3 4c6f72656d206970'];
       read(function(results) {
-
-        assert.equal(results[0], files[0]);
-        assert.equal(results[1], files[1]);
-        assert.equal(results[3], files[3]);
-
+        assert.deepEqual(results, files);
         done();
       });
     });
