8748d0d2a7614041d2e9ddeb998220624891ce39
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-11-22 00:55:18 -0800

    reformatted so test passes properly

--- lab-carolina/test/test.js
+++ lab-carolina/test/test.js
@@ -3,9 +3,11 @@ const assert = require('assert');
 
 describe('testing module asyncly', function(){
   describe('read()', function(){
-    it('should return files in order', function(done){
+    it('should return .txt files in order', function(done){
       read(function(text){
-        assert.equal(text[ '73747566662073749999', '6a756e6b206a756e', '626c6168200a' ]);
+        assert.equal(text[0], '7374756666207374');
+        assert.equal(text[1], '6a756e6b206a756e');
+        assert.equal(text[2], '626c6168200a');
         done();
       });
     });
