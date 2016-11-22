3ec9b684e15605141b63a6281d1cd05a3a32f3f0
Frank Iriarte <jurrish@gmail.com>
2016-11-22 14:22:33 -0800

    cant test asynch code like this

---
+++ index.js
@@ -0,0 +1,1 @@
+var runTheThings = require('')
--- test/hex-test.js
+++ test/hex-test.js
@@ -2,15 +2,23 @@
 
 let assert = require('assert');
 let fs = require('fs');
+let readAll = require('../lib/reader');
 
-describe('should be a fs method that prints string', function() {
-  it('if no error, it should stdout string from one.txt', function() {
-    assert.equal(fs.readFile('./lib/one.txt', function(err, data) {
-      if(err) {
-        process.stdout.write(err);
-        return;
-      }
-      process.stdout.write(data.toString());
-    }));
+//don't test other libraries
+// describe('should be a fs method that prints string', function() {
+//   it('if no error, it should stdout string from one.txt', function() {
+//     assert.equal(fs.readFile('./lib/one.txt', function(err, data) {
+//       if(err) {
+//         process.stdout.write(err);
+//         return;
+//       }
+//       process.stdout.write(data.toString());
+//     }));
+//   });
+// });
+
+describe('should return an array', function() {
+  it('should return an array', function() {
+    assert.equal(Array.isArray(readAll()), true);
   });
 });
