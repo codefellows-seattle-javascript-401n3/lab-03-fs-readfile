1a68b35020bf466cab589d371d8e661d96ee8599
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-11-21 23:10:56 -0800

    clean up

--- lab-jessica/test/test.js
+++ lab-jessica/test/test.js
@@ -2,7 +2,6 @@ const assert = require('assert');
 const read = require('../lib/read.js');
 
 describe('Read Module', function() {
-
   describe('#read()',  function() {
     it('files logged in correct order', function(done) {
       const files = ['file 1 5b0a20207b0a2020', 'file 2 6c61622d30332d66', 'file 3 4c6f72656d206970'];
