1bc03adc6c567c9a28b3e576e1fbb75495d2500e
Kyle Winckler <kyle.winckler@gmail.com>
2016-11-23 16:52:40 -0800

    refactor to include recursive function

--- lab-kyle/lib/read.js
+++ lab-kyle/lib/read.js
@@ -2,71 +2,31 @@
 
 const fs = require('fs');
 
-
-function readFileOne(file, file2, file3, results, callback) {
-  fs.readFile(file, function(err, data) {
-    if (err) throw err;
-    let formatted = data.slice(0,8).toString('hex');
-    results.push(formatted);
-    console.log(formatted);
-    readFileTwo(file2, file3, results, callback);
-  });
-}
-
-function readFileTwo(file2, file3, results, callback) {
-  fs.readFile(file2, function(err, data) {
-    if (err) throw err;
-    let formatted = data.slice(0,8).toString('hex');
-    results.push(formatted);
-    console.log(formatted);
-    readFileThree(file3, results, callback);
-  });
-}
-
-function readFileThree(file3, results, callback) {
-  fs.readFile(file3, function(err, data) {
-    if (err) throw err;
-    let formatted = data.slice(0,8).toString('hex');
-    results.push(formatted);
-    console.log(formatted);
+function readAFile(files, results, callback) {
+  if (files.length === 0) {
     callback(results);
-  });
+  } else {
+    fs.readFile(files[0], function(err, data) {
+      if (err) throw err;
+      files.shift();
+      let formatted = data.slice(0,8).toString('hex');
+      results.push(formatted);
+      console.log(formatted);
+      readAFile(files, results, callback);
+    });
+  }
 }
 
 function returnShit(results) {
-  console.log(results);
   return results;
 }
 
-function readThreeFiles(file, file2, file3, callback) {
+function readFiles(files, callback) {
   let results = [];
-  readFileOne(file, file2, file3, results, callback);
+  readAFile(files, results, callback);
 }
 
 module.exports = {
-  readThreeFiles: readThreeFiles,
+  readFiles: readFiles,
   returnShit: returnShit
 };
-
-// function reader(file, file2, file3, callback) {
-//   let results = [];
-//   fs.readFile(file, function(err, data) {
-//     if (err) throw err;
-//     let formatted = data.slice(0,8).toString('hex');
-//     results.push(formatted);
-//     console.log(formatted);
-//     fs.readFile(file2, function(err, data) {
-//       if (err) throw err;
-//       let formatted = data.slice(0,8).toString('hex');
-//       results.push(formatted);
-//       console.log(formatted);
-//       fs.readFile(file3, function(err, data) {
-//         if (err) throw err;
-//         let formatted = data.slice(0,8).toString('hex');
-//         results.push(formatted);
-//         console.log(formatted);
-//         callback(results);
-//       });
-//     });
-//   });
-// }
