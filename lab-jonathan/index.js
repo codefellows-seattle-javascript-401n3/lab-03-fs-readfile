8198a21d7e2e5f2524981c7e850f4ad6e0729ce9
spamalope01 <spamalope01@yahoo.com>
2016-12-07 09:46:54 -0800

    made corrections based on scott feedback.  resubmitting assignment for full credit.

--- lab-jonathan/index.js
+++ lab-jonathan/index.js
@@ -1,23 +1,8 @@
 let hex = require('./lib/readHex.js');
 
-hex.read(function(data) {
-  for(var i = 0; i < data.length; i++) {
-    console.log(data[i]);
-  }
-  // console.log("data: " + data);
+hex.readHex(function(data) {
+  data.forEach(function(ele){
+    console.log(ele);
+
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// * You will need to write tests that guarantee that the files are logged in order.
-// * You will also need to look up and implement async testing, making sure that you are not getting "false positives" (tests that are passing but shouldn't be).
-//  * hint read mocha docs
