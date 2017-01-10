6b9fccb2d32ab4da1c45ff1c9e55a2b79851b596
Michael Bishop <polishthebishop@gmail.com>
2017-01-09 20:12:31 -0800

    forgot semi-colon

--- lab-michael/lib/fs-readfile.js
+++ lab-michael/lib/fs-readfile.js
@@ -1,26 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
 
 module.exports = function(cb) {
   var textArray = [];
   fs.readFile(__dirname + '/../data/one.txt', function(err, data) {
-    if (err) {
-      console.log(err)
-    }
+    if (err) console.error(err);
     textArray.push(data.toString('hex', 0, 8));
-    fs.readFile(__dirname + '/../data/two.txt', function(err, data) {
-      if (err) {
-        console.log(err)
-      }
-      textArray.push(data.toString('hex', 0, 8));
-      fs.readFile(__dirname + '/../data/three.txt', function(err, data) {
-        if (err) {
-          console.log(err)
-        }
-        textArray.push(data.toString('hex', 0, 8));
-        // console.log('MAIN FILE');
-        cb(textArray);
-      })
-    })
-  })
 
-}
+    fs.readFile(__dirname + '/../data/two.txt', function(err, data) {
+      if (err) console.error(err);
+      textArray.push(data.toString('hex', 0, 8));
+
+      fs.readFile(__dirname + '/../data/three.txt', function(err, data) {
+        if (err) console.error(err);
+        textArray.push(data.toString('hex', 0, 8));
+        cb(textArray);
+      });
+    });
+  });
+
+};
