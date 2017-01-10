5dae907443960e37a960c86df49592113c8b3a3b
Michael Bishop <polishthebishop@gmail.com>
2017-01-09 20:08:35 -0800

    console error on one line

--- lab-michael/lib/fs-readfile.js
+++ lab-michael/lib/fs-readfile.js
@@ -1,25 +1,26 @@
-const fs = require('fs');
+const fs = require('fs')
 
 module.exports = function(cb) {
   var textArray = [];
   fs.readFile(__dirname + '/../data/one.txt', function(err, data) {
     if (err) {
-      console.log(err);
+      console.log(err)
     }
     textArray.push(data.toString('hex', 0, 8));
     fs.readFile(__dirname + '/../data/two.txt', function(err, data) {
       if (err) {
-        console.log(err);
+        console.log(err)
       }
       textArray.push(data.toString('hex', 0, 8));
       fs.readFile(__dirname + '/../data/three.txt', function(err, data) {
         if (err) {
-          console.log(err);
+          console.log(err)
         }
         textArray.push(data.toString('hex', 0, 8));
+        // console.log('MAIN FILE');
         cb(textArray);
-      });
-    });
-  });
+      })
+    })
+  })
 
-};
+}
